2d45f9be0b0a7ad7296a00d28dc7c936efae6129
Onixfff <danilnikonov01@mail.ru>
2024-06-02 15:42:17 +0200

    Вынес классы

---
+++ TelegramMessager/Data.cs
@@ -0,0 +1,20 @@
+﻿namespace TelegramMessager
+{
+    public class Data
+    {
+        public string Date { get; private set; }
+        public EnumDayOrNight IsDay { get; private set; }
+        public string Text { get; private set; }
+        public int Count { get; private set; }
+        public double LongCount {  get; private set; }
+
+        public Data(string date, EnumDayOrNight isDay, string text, int count, double longCount)
+        {
+            Date = date;
+            IsDay = isDay;
+            Text = text;
+            Count = count;
+            LongCount = longCount;
+        }
+    }
+}
---
+++ TelegramMessager/DataMount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TelegramMessager
+{
+    public class DataMount
+    {
+        public string Text { get; private set; }
+        public int Count { get; private set; }
+        public double LongCount { get; private set; }
+
+        private DateTime _byDate;
+        private DateTime _fromDate;
+
+        public DataMount(string text, int count, double longCount)
+        {
+            Text = text;
+            Count = count;
+            LongCount = longCount;
+        }
+
+        public void AddByDate(DateTime date)
+        {
+            _byDate = date;
+        }
+
+        public void AddFromDate(DateTime date)
+        {
+            _fromDate = date;
+        }
+
+        public DateTime GetFromDate()
+        {
+            return _fromDate;
+        }
+
+        public DateTime GetByDate()
+        {
+            return _byDate;
+        }
+    }
+}
